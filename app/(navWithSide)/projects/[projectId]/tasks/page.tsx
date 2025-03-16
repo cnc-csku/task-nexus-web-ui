82c36847654d614cea73309cefb0ad6f747f463d
Chayakorn Chiensuwikarn <98451932+Chayakorn2002@users.noreply.github.com>
2025-03-16 16:14:21 +0700

    feat: implement Create Task modal. (#2)

--- app/(navWithSide)/projects/[projectId]/tasks/page.tsx
+++ app/(navWithSide)/projects/[projectId]/tasks/page.tsx
@@ -1,14 +1,35 @@
+"use client";
 import Header from '@/components/ui/Header'
 import React from 'react'
 import TasksTable from './_components/TasksTable'
+import CreateTaskModal from './_components/CreateTaskModal'
+import { useDisclosure } from '@heroui/modal'
+import { Button } from '@heroui/button'
 
 export default function ProjectTaskPage() {
+  const { isOpen, onOpen, onOpenChange } = useDisclosure();
+
   return (
     <div>
-        <Header>
-            Tasks
-        </Header>
-        <TasksTable />
+      <Header>
+        Tasks
+      </Header>
+      <div className="flex mb-2 justify-between items-center">
+        <div>
+          <Button
+            variant="flat"
+            color="primary"
+            onPress={onOpen}
+          >
+            Create Task
+          </Button>
+        </div>
+      </div>
+      <TasksTable />
+      <CreateTaskModal
+        isOpen={isOpen}
+        onOpenChange={onOpenChange}
+      />
     </div>
   )
 }
